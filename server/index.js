9e2525fc5e218dd494be4825688386f2d987deca
Navneet Sheokand <youremail@example.com>
2026-02-15 12:34:19 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://smartysols-kp12v6x1f-navneeys-projects.vercel.app"
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
